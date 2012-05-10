af239c4d6b2eeb4a60f6025d7898ad85cfb15109
qwe <qwe@qwe.com>
2012-05-10 15:11:11 -0600

    delete the git.pl file

--- git.pl
+++
@@ -1,9 +0,0 @@
-my $command = qq{git log -1 --pretty=format:"%H|%an|%s|%cd"};
-           
-my $logResult = qx{$command};
-$logResult =~ m/([a-z0-9]+)\|(.*)\|(.*)\|(.*)/gi;
-
-my $snapshot = $1;
-my $changelog = join("\n","Author: $2","summary: $3", "date: $4")."\n";
-
-print $changelog;
